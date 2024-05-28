65c0af23eab44aa6279601325f0f17ff63c32f04
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-28 11:55:16 +0530

    feat: Add basic web server

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import(
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("go-redis-microservice")
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to port :3000", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("go-microservice"))
 }
