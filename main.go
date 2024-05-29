b7624fe98c620ffebfd44aa4007bc28e944cbe2c
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-05-30 02:04:31 +0530

    feat: add graceful shutdown on OS interrupt signal

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/Vkanhan/go-redis-microservice/application"
 )
@@ -12,8 +14,11 @@ func main() {
 	//create an instance of the application
 	app := application.New()
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
 	// Start the application by calling the Start method with a background context
-	err := app.Start(context.TODO())
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app: ", err)
 	}
