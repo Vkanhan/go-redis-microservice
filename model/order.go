fa16e82283eb55e42e83bbcb346e7f788edc9999
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-06-03 23:33:53 +0530

    fix: add pointer to time.Time

--- model/order.go
+++ model/order.go
@@ -6,15 +6,17 @@ import (
 	"github.com/google/uuid"
 )
 
+// Order represents a customer's order
 type Order struct {
 	OrderID     uint64      `json:"order_id"`
 	CustomerID  uuid.UUID   `json:"customer_id"`
 	LineItems   []LineItems `json:"line_items"`
-	CreatedAt   time.Time   `json:"created_at"`
-	ShippedAT   time.Time   `json:"shipped_at"`
-	CompletedAt time.Time   `json:"completed_at"`
+	CreatedAt   *time.Time   `json:"created_at"`
+	ShippedAT   *time.Time   `json:"shipped_at"`
+	CompletedAt *time.Time   `json:"completed_at"`
 }
 
+// LineItems represents an item in an order
 type LineItems struct {
 	ItemID   uuid.UUID `json:"item_id"`
 	Price    uint      `json:"price"`
