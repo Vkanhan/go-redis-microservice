5ea0ec8c65ce08665f86df7be4b653532292371b
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-06-06 23:24:53 +0530

    refactor(application): integrate configuration into app initialization

--- application/app.go
+++ application/app.go
@@ -9,26 +9,34 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-//App struct stores the router to the http.Handler
+// App struct stores the router to the http.Handler
 type App struct {
-	router http.Handler	// router for handling HTTP requests
-	rdb *redis.Client	// Redis client for database operations
+	router http.Handler  // router for handling HTTP requests
+	rdb    *redis.Client // Redis client for database operations
+	config Config        //configuration for the app
 }
 
 // New creates a new instance of the App struct, initializes the router, and returns the instance.
-func New() *App {
+func New(config Config) *App {
 	// Initialize the App struct with the router configured by loadRoutes
 	app := &App{
-		router: loadRoutes(),					  // Initialize router
-		rdb: redis.NewClient(&redis.Options{}),	 // Initialize Redis client
+		rdb: redis.NewClient(&redis.Options{ // Initialize Redis client with provided configuration
+			Addr: config.RedisAddress,
+		}),
+		config: config,
 	}
+
+	// Load routes for the application
+	app.loadRoutes()
+
 	return app
 }
 
 // Start starts the HTTP server with the given context.
 func (a *App) Start(ctx context.Context) error {
+	// Create a new HTTP server with the specified address and handler
 	server := http.Server{
-		Addr: ":3000",
+		Addr:    fmt.Sprintf(":%d", a.config.ServerPort),  // Use server port from configuration
 		Handler: a.router,
 	}
 
@@ -62,17 +70,16 @@ func (a *App) Start(ctx context.Context) error {
 
 	// Select statement to handle either server error or context cancellation
 	select {
-	case err = <- ch:
+	case err = <-ch:
 		return err
-	case <- ctx.Done():
+	case <-ctx.Done():
 		// If the context is cancelled, initiate server shutdown
 		// Create a new context with a timeout for the shutdown process
-		timeout, cancel := context.WithTimeout(context.Background(), time.Second * 10)
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
 		defer cancel()
 
-		//shut down the server gracefully
+		//shut down the server gracefully within the timeout period
 		return server.Shutdown(timeout)
 	}
-	
-}
 
+}
